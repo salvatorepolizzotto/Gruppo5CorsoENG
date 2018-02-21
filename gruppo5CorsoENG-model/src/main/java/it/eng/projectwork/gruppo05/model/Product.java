ee240b2b4b5362eda91819336ba28a048170f772
gerla_000 <gerla_000@192.168.28.130>
2018-02-21 17:04:16 +0100

    Modifica versione definitiva Product

--- gruppo5CorsoENG-model/src/main/java/it/eng/projectwork/gruppo05/model/Product.java
+++ gruppo5CorsoENG-model/src/main/java/it/eng/projectwork/gruppo05/model/Product.java
@@ -1,23 +1,40 @@
 package it.eng.projectwork.gruppo05.model;
 
 import javax.persistence.Column;
+import java.io.Serializable;
 import javax.persistence.Transient;
 import javax.persistence.Table;
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Id;
 import java.awt.*;
 import java.util.Collection.*;
 
 @Entity
 @Table(name="PRODUCT")
-public class Product {
+public class Product implements Serializable {
+	
 	@Id
-	@Column(name="PROD_ID")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PROD_ID")
+	@SequenceGenerator(name="PROD_ID", sequenceName="PROD_ID", allocationSize=1)
 	private long id;
 	
 	@Column(name="PROD_DESCR")
 	private String description;
-
+	
+	//Costruttori
+	
+	public Product(){}
+	
+	public Product(long id, String descr) {
+		this.id = id;
+		this.description = descr;
+	}
+	
+	//Getters e Setters
+	
 	public long getId() {
 		return id;
 	}
